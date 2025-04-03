a5ff4fea45d587ee41992adbc603dd08e9f0aa35
manmohanaeir1 <manmohanaeir001110@gmail.com>
2025-04-03 15:32:27 +0545

    get single data and update data using primary key  Class based view through API Endpoint

--- api/views.py
+++ api/views.py
@@ -103,3 +103,12 @@ class EmployeeDetail(APIView):
           serializer = EmployeeSerializer(employee)
           return Response(serializer.data, status=status.HTTP_200_OK)
            
+
+     def put(self, request, pk):
+          employee = self.get_object(pk) # get the employee object by id
+          serializer = EmployeeSerializer(employee, data=request.data)      #
+          if serializer.is_valid():
+                 serializer.save()
+                 return Response(serializer.data, status=status.HTTP_200_OK)
+          return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+          
