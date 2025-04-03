477535848687a0285996b662d244b998432958b8
manmohanaeir1 <manmohanaeir001110@gmail.com>
2025-04-03 12:00:41 +0545

    fetch and store data using API endpoint

--- api/views.py
+++ api/views.py
@@ -18,11 +18,21 @@ def studentsView(request):
 
     
 # Automatic Serialization
-@api_view(['GET'])
+@api_view(['GET', 'POST']) # this decorator is used to specify the allowed methods for the view
 def studentsView(request):
      if request.method == 'GET':
             # get all students from the database
          students = Student.objects.all()
          serializer = StudentSerializer(students, many=True) # many=True is used when we want to serialize multiple objects
          return Response(serializer.data, status=status.HTTP_200_OK)
+     elif request.method == 'POST':
+            serializer = StudentSerializer(data=request.data) # data=request.data is used to get the data from the request
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+            print(serializer.errors)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)    
+
+         
      
