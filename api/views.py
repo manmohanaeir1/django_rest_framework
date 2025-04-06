44b04f287ba8cdfb34f660755bed9078d90cf646
manmohanaeir1 <manmohanaeir001110@gmail.com>
2025-04-06 18:46:59 +0545

    Viewset imolementation in DFR for retreive single element and delete  by PK

--- api/views.py
+++ api/views.py
@@ -198,3 +198,8 @@ class EmployeesViewSet(viewsets.ViewSet):
                return Response(serializer.data, status=status.HTTP_200_OK)
           return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
              
+
+     def delete(self, request,pk=None):
+          employee = get_object_or_404(Employee, pk=pk)
+          employee.delete()
+          return Response(status=status.HTTP_204_NO_CONTENT)
