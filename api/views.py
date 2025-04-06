3cd8e886c38fd86d359817ee7fe7e136d49ddb93
manmohanaeir1 <manmohanaeir001110@gmail.com>
2025-04-06 18:34:03 +0545

    Viewset imolementation in DFR for create

--- api/views.py
+++ api/views.py
@@ -175,4 +175,9 @@ class EmployeesViewSet(viewsets.ViewSet):
           return Response(serializer.data, status=status.HTTP_200_OK)
      
 
-   
+     def create(self, request):
+            serializer = EmployeeSerializer(data=request.data)
+            if serializer.is_valid():
+                 serializer.save()
+                 return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
