15be1d6bddc1a26df4a0d40fa01b6de9cf38580a
manmohanaeir1 <manmohanaeir001110@gmail.com>
2025-04-08 23:31:06 +0545

    pagiation

--- django_rest_main/settings.py
+++ django_rest_main/settings.py
@@ -41,8 +41,7 @@ INSTALLED_APPS = [
     'students',
     'api',
     'employees',
-
-
+    'django_filters'
 
  ]
 
@@ -129,3 +128,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 2,  # Number of records per page
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+
+}
+ 
